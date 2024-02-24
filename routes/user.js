a2a9b848946cafc070370acc3d91b104be66d976
Vrishank Sharma <sharma08462@gmail.com>
2024-02-24 12:54:35 +0530

    Route.route method for users

--- routes/user.js
+++ routes/user.js
@@ -7,14 +7,18 @@ const { saveRedirectUrl } = require("../middleware.js");
 const userController = require("../controllers/users.js");
 const user = require("../models/user.js");
 
-router.get("/signup", userController.renderSignupForm)
 
-router.post("/signup", wrapAsync (userController.signup));
+router
+    .route("/signup")
+        .get( userController.renderSignupForm)
+        .post( wrapAsync (userController.signup))
 
-//login
-router.get("/login", userController.renderLoginForm)
 
-router.post("/login",saveRedirectUrl,passport.authenticate("local",{failureRedirect:'/login', failureFlash: true}),userController.login)
+router
+    .route("/login")
+        .get( userController.renderLoginForm) //login
+        .post( saveRedirectUrl,passport.authenticate("local",{failureRedirect:'/login', failureFlash: true}),userController.login)
+
 
 //logout
 router.get("/logout", userController.logout)
