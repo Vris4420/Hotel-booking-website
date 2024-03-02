f2fefbc040ce2339ebc7c90466551d56076804c6
Vrishank Sharma <sharma08462@gmail.com>
2024-03-02 21:50:17 +0530

    commit

--- routes/listing.js
+++ routes/listing.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const router = express.Router();             // creating new router object
-
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js")
 const{isLoggedIn, isOwner, validateListing} = require("../middleware.js")
 const listingController = require("../controllers/listing.js")
+// multer
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 router
     .route("/")
         .get( wrapAsync(listingController.index))  //Index Route
-        .post(isLoggedIn,validateListing, wrapAsync(listingController.createListing))  //Create Route
+       // .post(isLoggedIn,validateListing, wrapAsync(listingController.createListing))  //Create Route
+       .post(upload.single('listing[image]'),(req,res) => {
+        res.send(req.file)
+       })
 
 
 //New Listing
